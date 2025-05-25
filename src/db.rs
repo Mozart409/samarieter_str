77dde4fa33e0af3f285cd62608e8bf846339086f
Amadeus <amadeus@mozart409.com>
2025-05-25 19:37:49 +0200

    feat: update user and tenant functions to use correct structs and add item management

--- src/db.rs
+++ src/db.rs
@@ -5,7 +5,7 @@ use argon2::{
 
 use crate::{
     errors::AppError,
-    routes::{Tenants, User},
+    structs::{Item, Tenant, User},
     AppState,
 };
 
@@ -18,12 +18,24 @@ pub async fn get_all_users(state: &AppState) -> Result<Vec<User>, sqlx::Error> {
     Ok(users)
 }
 
-pub async fn get_all_tenants(state: &AppState) -> Result<Vec<Tenants>, sqlx::Error> {
+pub async fn get_all_tenants(state: &AppState) -> Result<Vec<Tenant>, sqlx::Error> {
     let pool = state.db_pool.clone();
-    let tenants = sqlx::query_as::<_, Tenants>("SELECT * FROM tenants")
+    let tenants = sqlx::query_as::<_, Tenant>("SELECT * FROM tenants")
         .fetch_all(&pool)
         .await?;
     Ok(tenants)
+}
+
+pub async fn get_user_by_email(state: &AppState, email: String) -> Result<User, sqlx::Error> {
+    let pool = state.db_pool.clone();
+    let user = sqlx::query_as::<_, User>("SELECT * FROM users WHERE email = $1")
+        .bind(email)
+        .fetch_optional(&pool)
+        .await?;
+    match user {
+        Some(u) => Ok(u),
+        None => Err(sqlx::Error::RowNotFound),
+    }
 }
 
 pub async fn get_user_by_id(state: &AppState, id: i64) -> Result<User, sqlx::Error> {
@@ -34,9 +46,9 @@ pub async fn get_user_by_id(state: &AppState, id: i64) -> Result<User, sqlx::Err
         .await?;
     Ok(user)
 }
-pub async fn get_tenant_by_id(state: &AppState, id: i64) -> Result<Tenants, sqlx::Error> {
+pub async fn _get_tenant_by_id(state: &AppState, id: i64) -> Result<Tenant, sqlx::Error> {
     let pool = state.db_pool.clone();
-    let tenant = sqlx::query_as::<_, Tenants>("SELECT * FROM tenants WHERE id = $1")
+    let tenant = sqlx::query_as::<_, Tenant>("SELECT * FROM tenants WHERE id = $1")
         .bind(id)
         .fetch_one(&pool)
         .await?;
@@ -70,10 +82,10 @@ pub async fn create_user(
     Ok(user)
 }
 
-pub async fn create_tenant(state: &AppState, tenant_name: String) -> Result<Tenants, sqlx::Error> {
+pub async fn create_tenant(state: &AppState, tenant_name: String) -> Result<Tenant, sqlx::Error> {
     let created_at = chrono::Utc::now().to_string();
     let pool = state.db_pool.clone();
-    let tenant = sqlx::query_as::<_, Tenants>(
+    let tenant = sqlx::query_as::<_, Tenant>(
         "INSERT INTO tenants ( created_at, updated_at, name) VALUES ($1, $2, $3) RETURNING *",
     )
     .bind(&created_at)
@@ -85,7 +97,7 @@ pub async fn create_tenant(state: &AppState, tenant_name: String) -> Result<Tena
     Ok(tenant)
 }
 
-pub async fn delete_user(state: &AppState, id: i64) -> Result<(), sqlx::Error> {
+pub async fn _delete_user(state: &AppState, id: i64) -> Result<(), sqlx::Error> {
     let pool = state.db_pool.clone();
     sqlx::query("DELETE FROM users WHERE id = $1")
         .bind(id)
@@ -94,7 +106,7 @@ pub async fn delete_user(state: &AppState, id: i64) -> Result<(), sqlx::Error> {
     log::info!("User with id {} deleted", id);
     Ok(())
 }
-pub async fn delete_tenant(state: &AppState, id: i64) -> Result<(), sqlx::Error> {
+pub async fn _delete_tenant(state: &AppState, id: i64) -> Result<(), sqlx::Error> {
     let pool = state.db_pool.clone();
     sqlx::query("DELETE FROM tenants WHERE id = $1")
         .bind(id)
@@ -155,4 +167,52 @@ pub async fn update_user(
 
     log::info!("User updated: {:?}", user);
     Ok(user)
+}
+
+pub async fn create_item(
+    state: &AppState,
+    tenant_id: i64,
+    name: String,
+    amount: String,
+) -> Result<(), sqlx::Error> {
+    let created_at = chrono::Utc::now().to_string();
+    let pool = state.db_pool.clone();
+    log::info!("Item created for tenant_id {}: {}", tenant_id, name);
+    sqlx::query("INSERT INTO items (tenant_id, created_at, updated_at, name, amount) VALUES ($1, $2, $3, $4, $5)")
+        .bind(tenant_id)
+        .bind(&created_at)
+        .bind(&created_at)
+        .bind(name)
+        .bind(amount)
+        .execute(&pool)
+        .await?;
+    Ok(())
+}
+
+pub async fn get_all_items(state: &AppState, tenant_id: i64) -> Result<Vec<Item>, sqlx::Error> {
+    let pool = state.db_pool.clone();
+    let items = sqlx::query_as!(Item, "SELECT * FROM items WHERE tenant_id = $1", tenant_id)
+        .fetch_all(&pool)
+        .await?;
+    log::info!("Items for tenant_id {}: {:?}", tenant_id, items);
+    Ok(items)
+}
+
+pub async fn delete_item(
+    state: &AppState,
+    tenant_id: i64,
+    item_id: i64,
+) -> Result<(), sqlx::Error> {
+    let pool = state.db_pool.clone();
+    sqlx::query("DELETE FROM items WHERE tenant_id = $1 AND id = $2")
+        .bind(tenant_id)
+        .bind(item_id)
+        .execute(&pool)
+        .await?;
+    log::info!(
+        "Item with id {} deleted for tenant_id {}",
+        item_id,
+        tenant_id
+    );
+    Ok(())
 }
